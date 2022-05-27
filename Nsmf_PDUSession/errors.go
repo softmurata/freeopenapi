7f9d16939451803d8d4b89e4f81600e69bfed047
softmurata <tatsuromurata317@gmail.com>
2022-05-27 08:23:43 +0000

    fix nsmf pdusession errors.go bug

--- Nsmf_PDUSession/errors.go
+++ Nsmf_PDUSession/errors.go
@@ -3,7 +3,8 @@ package Nsmf_PDUSession
 import (
 	"net/http"
 
-	"github.com/free5gc/openapi/models"
+	// "github.com/free5gc/openapi/models"
+	"github.com/softmurata/freeopenapi/models"
 )
 
 var (
