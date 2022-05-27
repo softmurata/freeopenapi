f15494dfe1645df4454e3479824a0d3cdbe6860c
softmurata <tatsuromurata317@gmail.com>
2022-05-27 02:49:46 +0000

    fix n1 message container bug

--- models/model_n1_message_container.go
+++ models/model_n1_message_container.go
@@ -2,6 +2,6 @@ package models
 
 type N1MessageContainer struct {
 	N1MessageClass   N1MessageClass   `json:"n1MessageClass"`
-	N1MessageContent *ReftoBinaryData `json:"n1MessageContent"`
+	N1MessageContent *RefToBinaryData `json:"n1MessageContent"`
 	NfId             string           `json:"nfId"`
 }
